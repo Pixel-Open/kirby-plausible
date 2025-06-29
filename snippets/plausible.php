f6cc8584eb8443107ebea0eb50a3b8efe44ff230
Matthieu <matthieu@pixel-developpement.com>
2025-06-29 09:54:47 +0200

    + snippets/plausible.php: Enhanced XSS protection with esc(), domain validation regex

--- snippets/plausible.php
+++ snippets/plausible.php
@@ -1,5 +1,11 @@
-<?php if(option('debug') !== true): ?>
-    <script defer data-domain="<?= parse_url(
-        $kirby->url('index')
-    )['host'] ?>" src="https://plausible.io/js/plausible.js"></script>
+<?php if (option('debug') !== true): ?>
+<?php
+    $siteUrl = $kirby->url('index');
+    $parsedUrl = parse_url($siteUrl);
+    $domain = $parsedUrl['host'] ?? '';
+    
+    if (!empty($domain) && preg_match('/^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/', $domain)):
+?>
+    <script defer data-domain="<?= esc($domain) ?>" src="https://plausible.io/js/plausible.js"></script>
+<?php endif; ?>
 <?php endif ?>
